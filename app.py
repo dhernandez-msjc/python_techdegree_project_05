2ac2d173bf27775b08ee0a5e9d971bca43ac8718
David C Hernandez <58623347+dhernandez-msjc@users.noreply.github.com>
2022-09-22 20:19:53 -0700

    updated application

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/detail')
+def detail():
+    return render_template('detail.html')
+
+
+@app.route('/projectform')
+def project_form():
+    return render_template('projectform.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.0.1')
